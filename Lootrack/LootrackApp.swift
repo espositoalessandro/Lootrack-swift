fa8f34a2c88f917d62586bd2d5435803f2ae1db2
Alessandro Esposito <alessandro.esposito251@gmail.com>
2026-08-13 17:04:09 +0200

    Initial Commit

---
+++ Lootrack/LootrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LootrackApp.swift
+//  Lootrack
+//
+//  Created by Alessandro Esposito on 13/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct LootrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
