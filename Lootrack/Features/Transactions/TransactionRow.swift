3db224bb5da5b01ec2f88f4bdcd03f0ccbbcfa56
Alessandro Esposito <alessandro.esposito251@gmail.com>
2026-08-14 17:58:43 +0200

    pizzaz

--- Lootrack/Features/Transactions/TransactionRow.swift
+++ Lootrack/Features/Transactions/TransactionRow.swift
@@ -1,20 +1,33 @@
 import SwiftUI
+import SwiftData
 
 struct TransactionRow: View {
     let transaction: Transaction
+    
+    @Query(CategoryQueries.activeByName)
+    private var categories: [Category]
+    
+    private var category: Category? {
+        guard let categoryId = transaction.categoryId else { return nil}
+        return categories.first(where: { $0.id == categoryId })
+    }
     
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
                 Text(transaction.note)
+                    .bold()
                 
-                Text(transaction.type == .expense ? "Expense" : "Income")
+                Text(category != nil ? category!.name : "Uncategorized")
                     .foregroundStyle(.secondary)
             }
             
             Spacer()
             
-            Text("\(transaction.amountInCents)")
+            Text(((transaction.amountInCents)/100).formatted(.currency(code: "EUR")))
+                .bold()
+                .foregroundStyle(transaction.type == .expense ? .red : .green)
+
         }
     }
 }
