12e6471535b613ec38bb4959ec509901c07bf30a
Alessandro Esposito <alessandro.esposito251@gmail.com>
2026-08-14 11:31:04 +0200

    first steps

---
+++ Lootrack/AddTransactionView.swift
@@ -0,0 +1,25 @@
+//
+//  AddTransactionView.swift
+//  Lootrack
+//
+//  Created by Alessandro Esposito on 13/08/2026.
+//
+
+
+import SwiftUI
+
+struct AddTransactionView: View {
+    var body: some View {
+        VStack {
+            Text("New Transaction")
+                .font(.largeTitle)
+
+            Text("We're in a sheet!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    AddTransactionView()
+}
