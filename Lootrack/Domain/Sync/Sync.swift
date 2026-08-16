05bc20f04f3ed1c1d187ef96a50064abf862a67a
Alessandro Esposito <alessandro.esposito251@gmail.com>
2026-08-16 12:33:48 +0200

    asd

--- Lootrack/Domain/Sync/Sync.swift
+++ Lootrack/Domain/Sync/Sync.swift
@@ -16,11 +16,60 @@ nonisolated protocol Syncable {
     var lastMutationId: UUID? { get set }
 }
 
+func encode<T: Encodable>(_ value: T) throws -> Data {
+    return try JSONEncoder().encode(value)
+}
+
+func mutationFrom<T: Entity>(
+    _ entity: T,
+    previous: T?,
+    operation: SyncOperation
+) throws -> SyncMutation {
+
+    var mutationId = UUID()
+    let entityType: SyncEntityType
+    let payload: Data
+    let basePayload: Data?
+
+    switch entity {
+    case let transaction as Transaction:
+        entityType = .transaction
+        transaction.lastMutationId = mutationId
+        payload = try encode(TransactionDTO(transaction: transaction))
+        basePayload =
+            previous != nil
+            ? try encode(TransactionDTO(transaction: previous as! Transaction))
+            : nil
+
+    case let category as Category:
+        entityType = .category
+        category.lastMutationId = mutationId
+        payload = try encode(CategoryDTO(category: category))
+        basePayload =
+            previous != nil
+            ? try encode(CategoryDTO(category: previous as! Category)) : nil
+
+    default:
+        fatalError("Unsupported entity type: \(type(of: entity))")
+    }
+
+    return .init(
+        id: mutationId,
+        entityType: entityType,
+        entityId: entity.id,
+        operation: operation,
+        expectedRevision: previous?.revision,
+        expectedMutationId: previous?.lastMutationId,
+        basePayload: basePayload,
+        payload: payload,
+    )
+}
+
 @Model
 final class SyncMutation {
     @Attribute(.unique)
     var id: UUID
-    
+
     var entityType: SyncEntityType
     var entityId: UUID
     var operation: SyncOperation
