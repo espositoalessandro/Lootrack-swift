03dc8ac7c281db801dc71fb7878e326547d82d63
Alessandro Esposito <alessandro.esposito251@gmail.com>
2026-08-20 22:47:48 +0200

    add cat

--- Lootrack/Features/Categories/AddCategory.swift
+++ Lootrack/Features/Categories/AddCategory.swift
@@ -2,12 +2,11 @@ import SwiftUI
 
 struct AddCategoryView: View {
     @Environment(\.dismiss) private var dismiss
-
-    @Environment(CategoryService.self)
-    private var categoryService
-
+    @Environment(CategoryService.self) private var categoryService
+    
     @State private var draft = CategoryDraft()
-
+    @State private var error: CategoryServiceError?
+    
     var body: some View {
         NavigationStack {
             CategoryForm(draft: $draft)
@@ -18,7 +17,7 @@ struct AddCategoryView: View {
                             dismiss()
                         }
                     }
-
+                    
                     ToolbarItem(placement: .confirmationAction) {
                         Button("Save") {
                             save()
@@ -27,18 +26,21 @@ struct AddCategoryView: View {
                     }
                 }
         }
+        .alert(error: $error) { _ in
+            Button("OK") {}
+        } message: { error in
+            if let recoverySuggestion = error.recoverySuggestion {
+                Text(recoverySuggestion)
+            }
+        }
     }
-
+    
     private func save() {
         do {
-            try categoryService.create(name: draft.name,
-                                       type: draft.type,
-                                       note: draft.note)
-
+            try categoryService.create(name: draft.name, type: draft.type, note: draft.note)
             dismiss()
         } catch {
-            print("FAILED TO CREATE CATEGORY:",
-                  error)
+            self.error = error as? CategoryServiceError ?? .couldNotCreate
         }
     }
 }
